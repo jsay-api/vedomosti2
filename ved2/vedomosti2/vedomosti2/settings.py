687f08c3c69fce3d735fba0689d1a9caa82f0137
jsay-api <julia.sayapina@me.com>
2016-08-03 17:38:41 +0300

    mysql db connected
    herey!! finally managed to create a mysql DB and connect it to the
    project. good luck to me!

--- ved2/vedomosti2/vedomosti2/settings.py
+++ ved2/vedomosti2/vedomosti2/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Offshores',
+    'Users',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -81,10 +82,22 @@ WSGI_APPLICATION = 'vedomosti2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ved2',
+        'USER': 'root',
+        'PASSWORD': '0013Tau',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
